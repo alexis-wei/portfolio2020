34d50bda6b76250056d597310be059fbacc18c14
Alexis <qq_wei369@hotmail.com>
2020-04-30 10:33:48 -0400

    first commit

---
+++ frontend-2020/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <body>
+        <h1>Hi, I'm Alexis!</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;

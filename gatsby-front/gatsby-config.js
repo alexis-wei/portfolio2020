7f868f0773341c12e599a2bb0a13197f78425af9
Alexis <qq_wei369@hotmail.com>
2020-05-25 22:57:43 -0400

    add clouds

--- gatsby-front/gatsby-config.js
+++ gatsby-front/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Alexis Wei`,
+    title: `Portfolio`,
     description: `This is Alexis Wei`,
     author: `@alexis.w`,
   },
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bunny.svg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
--- gatsby-front/src/pages/index.js
+++ gatsby-front/src/pages/index.js
@@ -1,27 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Img from "gatsby-image"
 import SEO from "../components/seo"
-import Wave from "../images/wave.svg"
+import Wave1 from "../images/wave1.svg"
+import Wave2 from "../images/wave2.svg"
+import Cloud1 from "../images/cloud1.svg"
+import Cloud2 from "../images/cloud2.svg"
 import Grid from '@material-ui/core/Grid'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Hi There" />
+class IndexPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {img: Wave1}
+  }
 
-    <Grid container spacing={3}>
-      <Grid item xs={0} sm={2}/>
-      <Grid item xs={12} sm={8}>
-        <img src={Wave} alt="Alexis Waving" class="center" style={{width: "500px",paddingTop: "220px", paddingBottom: "100px"}}/>
-        <h4>I am an engineer, designer, innovator</h4>
-      </Grid>
-      <Grid item xs={0} sm={2}/>
-    </Grid>
-    
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
-)
+  render() {
+    return(
+      <Layout>
+        <SEO title="Alexis Wei" />
+
+        
+        <Grid container spacing={3}>
+          <Grid item xs={0} sm={2}/>
+          <Grid item xs={12} sm={8}>
+
+            <div style={{paddingTop: "140px", paddingBottom: "500px", position: "relative", justifyContent: "center",alignItems: "center"}}>
+              <img src={this.state.img} onMouseOver={() => {this.setState({img: Wave2})}} onMouseOut={() => {this.setState({img: Wave1})}} 
+              onFocus={ () => void 0 } onBlur={ () => void 0 } alt="Alexis Waving" className="center" style={{width: "500px", margin: "auto"}}/>
+              <img src={Cloud1} id="cloud-l" alt="Pink Cloud 1" style={{height: "200px", paddingRight: "200px", top:"100px", position:"absolute" }}/>
+              <img src={Cloud2} id="cloud-r" alt="Pink Cloud 2" style={{height: "400px", paddingLeft: "200px", top:"280px", position:"absolute" }}/>
+            </div>
+          </Grid>
+          <Grid item xs={0} sm={2}/>
+        </Grid>
+        
+        {/* <Link to="/page-2/">Go to page 2</Link> */}
+      </Layout>
+    )
+  }
+  
+}
 
 export default IndexPage

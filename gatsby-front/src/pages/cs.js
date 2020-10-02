31d16064ba7993f50916e9d8aae4538002a99c2c
alexis-wei <qq_wei369@hotmail.com>
2020-10-02 15:28:24 -0700

    more cs content

--- gatsby-front/src/pages/cs.js
+++ gatsby-front/src/pages/cs.js
@@ -49,8 +49,21 @@ class Cs extends React.Component {
                     <h2>FORMULA ELECTRIC @ BERKELEY</h2>
                     <h3>FIRMWARE DEVELPER</h3>
                     <p>Recently, I've switched from the Chassis team to the EECS team in order to help making this amazing vehicle move</p>
-                    <p>Now I am working with STM32CubeIDE and C++!</p>
+                    <p>Now I am working with STM32CubeIDE and C++ and definitely learning a ton!</p>
                       
+                  </Grid>
+                </Grid>
+                <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: "1.2rem"}}>
+                  <Grid item xs={12} md={6} lg={7} style={{backgroundColor: "#F4F4F4", padding:"3.5rem", borderRadius:"16px"}}>
+                    <h2>OM NOM</h2>
+                    <h3>HACK: NOW - PROJECT</h3>
+                    <p>At the start of COVID-19, a couple of friends and I wanted to created a social media platform for people who simply love food.
+                      There are so so many out there, but not a single central place for everyone to gather together, from people who love to cook, who love to eat,
+                      and every restaurants that have amazing dishes that they wish to share with the world. 
+                    </p>
+                    <p>In just 36 hours, we created a demo project in React.js and MongoDB that shows of a bit of what we hope to see come alive in the future.
+                    </p>
+                    <a href="https://www.youtube.com/watch?v=wV-2sYjasDs"  rel="noopener noreferrer"  target="_blank">Here's a short video of what we created</a>
                   </Grid>
                 </Grid>
             </div>
